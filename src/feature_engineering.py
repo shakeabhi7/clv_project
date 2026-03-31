d275beda07c05b6e1f0f62610f01f24d0f8527ea
shakeabhi7 <kumarabhishekt7@gmail.com>
2026-03-31 16:47:14 +0530

    Removing unwanted libraries

--- src/feature_engineering.py
+++ src/feature_engineering.py
@@ -1,8 +1,5 @@
 import pandas as pd
-import numpy as np
 import matplotlib.pyplot as plt
-import seaborn as sns
-from datetime import datetime
 import warnings
 import os
 
--- src/model.py
+++ src/model.py
@@ -1,9 +1,7 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import seaborn as sns
 import warnings
-import os
 import pickle
 from datetime import datetime
 
